2ef261559509aebdbf9e646812be3a2fcbd40fef
Kyle Johnson <kyle@solidstategroup.com>
2021-03-01 18:54:52 +0000

    Handle CMTIME_IS_INDEFINITE for duration changes

--- ios/AwsIvsAdapterPlayerView.m
+++ ios/AwsIvsAdapterPlayerView.m
@@ -90,10 +90,12 @@ static const NSInteger kDefaultMaxBufferTimeInSeconds = 10;
 }
 
 - (void)player:(IVSPlayer *)player didChangeDuration:(CMTime)duration {
-    NSLog(@"Changed duration to %@", @(CMTimeGetSeconds(duration)));
+    if (!CMTIME_IS_INDEFINITE(duration)) {
+        NSLog(@"Changed duration to %@", @(CMTimeGetSeconds(duration)));
 
-    if (self.onDidChangeDuration) {
-        self.onDidChangeDuration(@{@"duration": @(CMTimeGetSeconds(duration))});
+        if (self.onDidChangeDuration) {
+            self.onDidChangeDuration(@{@"duration": @(CMTimeGetSeconds(duration))});
+        }
     }
 }
 
